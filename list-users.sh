dd77397a8784da8a547962756917551c34da97b2
swarnavsgh <swarna.sfcrm@gmail.com>
2024-12-29 01:02:57 -0500

    Update list-users.sh

--- list-users.sh
+++ list-users.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+helper()
 # GitHub API URL
 API_URL="https://api.github.com"
 
@@ -36,6 +37,14 @@ function list_users_with_read_access {
     fi
 }
 
+function helper()
+{
+expected_cmd_args = 2
+if[$# ne expected_cmd_args]; then
+echo "Please execute the script with required command arguments"
+echo "asd"
+if
+}
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
